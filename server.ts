ae29eec684e7dfa9ecac74a08b341dd633b05acc
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2023-12-19 09:06:42 +0800

    fix: server crashing and not restarting when wrong password is input

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import generateAccessToken from "./utils/generateAccessToken";
-import cors from "cors"
-import { Request, Response } from 'express'
-import { Client } from "pg"
+import cors from "cors";
+import { Request, Response } from "express";
+import { Client } from "pg";
 import * as dotenv from "dotenv";
 import { compare, hash } from "bcrypt";
 
@@ -24,60 +24,60 @@ async function startServer() {
   app
     .use(cors())
     .use(express.json())
-    .post("/api/auth/login",  async (req : Request, res : Response) => {
-      const { email, password } = req.body
-      
+    .post("/api/auth/login", async (req: Request, res: Response) => {
+      const { email, password } = req.body;
+
       try {
-    
-        const response = await client.query(`
+        const response = await client.query(
+          `
         SELECT * FROM users WHERE email = $1
-        `, [email])
-        
+        `,
+          [email]
+        );
+
         const user = response.rows[0];
-    
+
         if (response.rows.length === 0) {
-          res.json({ message: "email or password is incorrect"})
+          res.json({ message: "email or password is incorrect" });
         }
-    
+
         const correctPassword = await compare(password, user.password);
-    
+
         if (!correctPassword) {
-          res.json({ message: "email or password is incorrect"})
+          res.json({ message: "email or password is incorrect" });
         }
-    
-        const token = generateAccessToken(email)
-    
+
+        const token = generateAccessToken(email);
+
         console.log(token);
         return res.json({ token: token });
       } catch (error) {
         console.log("Error:", error);
-        return res.json({ message: "failure", error: error });
       }
     })
-    .post("/api/auth/register", async (req : Request, res : Response) => {
-      const { userName, email, password } = req.body
+    .post("/api/auth/register", async (req: Request, res: Response) => {
+      const { userName, email, password } = req.body;
 
-      console.log(req.body)
+      console.log(req.body);
       try {
-    
-        const hashedPassword = await hash(password, 10)
+        const hashedPassword = await hash(password, 10);
 
-        console.log(hashedPassword)
-        console.log("ok ok ok")
-    
+        console.log(hashedPassword);
+        console.log("ok ok ok");
+
         const response = await client.query(
           `
           INSERT INTO users (username, email, password)
           VALUES ($1, $2, $3)
           `,
           [userName, email, hashedPassword]
-        )
-    
-        console.log(response)
-        res.json({message: "success"})
+        );
+
+        console.log(response);
+        res.json({ message: "success" });
       } catch (error) {
         console.log("Error:", error);
-        res.json({message: "failure", error: error})
+        res.json({ message: "failure", error: error });
       }
     })
     .listen(3001, () => {
