f67462da59bec89af31da7692cefa2a014d46b47
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2023-12-31 12:47:18 +0800

    removed: console.logs

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -8,10 +8,7 @@ export default async function requireAuth(req: Request, res: Response, next: Nex
     token = null
   }
 
-  console.log(token)
-
   if (token === null) {
-    console.log("here")
     res.json({ authenticated: false, body: []})
   } else {
     next()
