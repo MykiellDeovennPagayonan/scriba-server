1e8b60299c92425e32e3c32beaa5a11ed24d5488
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-01-04 07:08:30 +0800

    added: study groups functionality and backend

--- routes/studyGroups.ts
+++ routes/studyGroups.ts
@@ -66,6 +66,95 @@ router
     res.json({ authenticated: true, body: result.rows });
     client.release()
   })
+  .post("/study-notes", async (req: Request, res: Response) => {
+    const { userId } = req.body
+    const client = await pool.connect()
+
+    const result = await client.query(`
+    SELECT study_notes.title, study_notes.id
+    from users
+    INNER JOIN study_notes
+    ON users.id = study_notes.user_id
+    WHERE (users.id = $1 AND study_notes.is_public = true)
+    `, [ userId ]
+    );
+
+    console.log(result.rows)
+    res.json({ authenticated: true, body: result.rows });
+    client.release()
+  })
+  .get("/study-notes/:id", async (req: Request, res: Response) => {
+    const studyGroupId = req.params.id;
+    const client = await pool.connect()
+
+    const result = await client.query(`
+    SELECT study_notes.title, topics.name as "topicName", study_notes.id as "studyNoteId"
+    from study_groups
+    INNER JOIN shared_notes
+    ON study_groups.id = shared_notes.study_group_id
+    INNER JOIN study_notes
+    ON shared_notes.study_note_id = study_notes.id
+    INNER JOIN study_note_topics
+    ON study_notes.id = study_note_topics.study_notes_id
+    INNER JOIN topics ON study_note_topics.topic_id = topics.id
+    WHERE study_groups.id = $1
+    `, [ studyGroupId ]
+    );
+
+    console.log(result.rows)
+    res.json({ authenticated: true, body: result.rows });
+    client.release()
+  })
+  .post("/shared-notes/new", async (req: Request, res: Response) => {
+    const { studyGroupId, studyNoteId } = req.body
+    const client = await pool.connect()
+
+    const result = await client.query(`
+    INSERT INTO shared_notes (study_group_id, study_note_Id)
+    VALUES ($1, $2)
+    `, [ studyGroupId, studyNoteId ]
+    )
+    
+    res.json({ authenticated: true, body: "yessss" });
+    client.release()
+  })
+  .delete("/shared-notes/delete", async (req: Request, res: Response) => {
+    const { id: sharedNoteID } = req.body
+    const client = await pool.connect()
+
+    console.log(sharedNoteID)
+
+    const result = await client.query(`
+    DELETE FROM shared_notes
+    WHERE id = $1
+    `, [ sharedNoteID ]
+    )
+    
+    res.json({ authenticated: true, body: result.rows });
+    client.release()
+  })
+  .post("/shared-notes/:id", async (req: Request, res: Response) => {
+    const { userId } = req.body
+    const studyGroupId = req.params.id;
+    const client = await pool.connect()
+
+    const result = await client.query(`
+    SELECT study_notes.id as "studyNoteId", shared_notes.id as "id"
+    FROM study_groups
+    INNER JOIN shared_notes
+    ON study_groups.id = shared_notes.study_group_id
+    INNER JOIN study_notes
+    ON study_notes.id = shared_notes.study_note_id
+    INNER JOIN users
+    ON study_notes.user_id = users.id
+    WHERE users.id = $1 AND study_groups.id = $2
+    `, [ userId, studyGroupId ]
+    )
+
+    console.log(result.rows)
+    res.json({ authenticated: true, body: result.rows });
+    client.release()
+  })
 
 function logger(req: Request, res: Response, next: NextFunction) {
   console.log(req.originalUrl, "logger");
--- routes/topics.ts
+++ routes/topics.ts
@@ -13,6 +13,14 @@ router
     res.json({ authenticated: true, body: result.rows });
     client.release()
   })
+  .get("/burgers", async (req: Request, res: Response) => {
+    const client = await pool.connect()
+    const result = await client.query(`
+    SELECT topics.name, topics.id from topics
+    `);
+    res.json({ authenticated: true, body: "I love burgers" });
+    client.release()
+  })
 
 
 module.exports = router;
