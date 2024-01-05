78f811ec29f5bdc2137c4e783ea3c6cb2c572bfc
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-01-05 09:54:24 +0800

    reworked: study group end point

--- routes/studyGroups.ts
+++ routes/studyGroups.ts
@@ -27,235 +27,294 @@ router
       `,
         [studyGroupId, userId]
       );
-      res.json({
-        message: "success",
-      });
+      res.status(201).json({ body: [] });
       client.release();
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error, body: [] });
     }
   })
-  .get("/", async (req: Request, res: Response) => {
-    try {
-      const client = await pool.connect();
-      const response = await client.query(`SELECT * FROM study_groups`);
-      const rows = response.rows;
+  // .get("/", async (req: Request, res: Response) => {
+  //   try {
+  //     const client = await pool.connect();
+  //     const response = await client.query(`SELECT * FROM study_groups`);
 
-      res.json(rows);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  })
+  //     res.status(200).json({ body: response.rows });
+  //     client.release();
+  //   } catch (error) {
+  //     console.log(error);
+  //     res.status(500).json({ error, body : [] })
+  //   }
+  // })
   .post("/", async (req: Request, res: Response) => {
     const { userId } = req.body;
-    const client = await pool.connect();
 
-    const result = await client.query(
-      `
-    SELECT study_groups.id as id, study_groups.name as name, study_groups.description as description
-    FROM study_groups
-    LEFT JOIN study_group_admins
-    ON study_groups.id = study_group_admins.study_group_id
-    LEFT JOIN study_group_members
-    ON study_groups.id = study_group_members.study_group_id
-    WHERE (study_group_members.user_id = $1 OR study_group_admins.user_id = $1)
-    `,
-      [userId]
-    );
+    try {
+      const client = await pool.connect();
 
-    console.log(result.rows);
-    res.json({ authenticated: true, body: result.rows });
-    client.release();
+      const result = await client.query(
+        `
+      SELECT study_groups.id as id, study_groups.name as name, study_groups.description as description
+      FROM study_groups
+      LEFT JOIN study_group_admins
+      ON study_groups.id = study_group_admins.study_group_id
+      LEFT JOIN study_group_members
+      ON study_groups.id = study_group_members.study_group_id
+      WHERE (study_group_members.user_id = $1 OR study_group_admins.user_id = $1)
+      `,
+        [userId]
+      );
+
+      res.status(200).json({ authenticated: true, body: result.rows });
+      client.release();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error, body: [] });
+    }
   })
   .post("/study-notes", async (req: Request, res: Response) => {
     const { userId } = req.body;
-    const client = await pool.connect();
 
-    const result = await client.query(
-      `
-    SELECT study_notes.title, study_notes.id
-    from users
-    INNER JOIN study_notes
-    ON users.id = study_notes.user_id
-    WHERE (users.id = $1 AND study_notes.is_public = true)
-    `,
-      [userId]
-    );
+    try {
+      const client = await pool.connect();
 
-    console.log(result.rows);
-    res.json({ authenticated: true, body: result.rows });
-    client.release();
+      const result = await client.query(
+        `
+      SELECT study_notes.title, study_notes.id
+      from users
+      INNER JOIN study_notes
+      ON users.id = study_notes.user_id
+      WHERE (users.id = $1 AND study_notes.is_public = true)
+      `,
+        [userId]
+      );
+  
+      res.status(200).json({ authenticated: true, body: result.rows });
+      client.release();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error, body: [] });
+    }
   })
   .get("/study-notes/:id", async (req: Request, res: Response) => {
     const studyGroupId = req.params.id;
-    const client = await pool.connect();
 
-    const result = await client.query(
-      `
-    SELECT
-      study_notes.title,
-		  topics.name as "topicName",
-		  study_notes.id as "studyNoteId",
-		  users.username as "userName"
-    FROM study_groups
-    INNER JOIN shared_notes
-    ON study_groups.id = shared_notes.study_group_id
-    INNER JOIN study_notes
-    ON shared_notes.study_note_id = study_notes.id
-    INNER JOIN study_note_topics
-    ON study_notes.id = study_note_topics.study_notes_id
-    INNER JOIN topics
-	  ON study_note_topics.topic_id = topics.id
-	  INNER JOIN users
-	  ON users.id = study_notes.user_id
-    WHERE study_groups.id = $1
-    `,
-      [studyGroupId]
-    );
+    try {
+      const client = await pool.connect();
 
-    res.json({ authenticated: true, body: result.rows });
-    client.release();
+      const result = await client.query(
+        `
+      SELECT
+        study_notes.title,
+        topics.name as "topicName",
+        study_notes.id as "studyNoteId",
+        users.username as "userName"
+      FROM study_groups
+      INNER JOIN shared_notes
+      ON study_groups.id = shared_notes.study_group_id
+      INNER JOIN study_notes
+      ON shared_notes.study_note_id = study_notes.id
+      INNER JOIN study_note_topics
+      ON study_notes.id = study_note_topics.study_notes_id
+      INNER JOIN topics
+      ON study_note_topics.topic_id = topics.id
+      INNER JOIN users
+      ON users.id = study_notes.user_id
+      WHERE study_groups.id = $1
+      `,
+        [studyGroupId]
+      );
+
+      res.status(200).json({ body: result.rows });
+      client.release();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error, body: [] });
+    }
   })
   .get("/members/:id", async (req: Request, res: Response) => {
     const studyGroupId = req.params.id;
-    const client = await pool.connect();
 
-    const result = await client.query(
-      `
-      SELECT users.id as "userID", users.username as "username" FROM users
-      INNER JOIN study_group_members
-      ON users.id = study_group_members.user_id
-      INNER JOIN study_groups 
-      ON study_groups.id = study_group_members.study_group_id
-      WHERE study_groups.id = $1
-    `,
-      [studyGroupId]
-    );
+    try {
+      const client = await pool.connect();
 
-    res.json({ authenticated: true, body: result.rows });
-    client.release();
+      const result = await client.query(
+        `
+        SELECT users.id as "userID", users.username as "username" FROM users
+        INNER JOIN study_group_members
+        ON users.id = study_group_members.user_id
+        INNER JOIN study_groups 
+        ON study_groups.id = study_group_members.study_group_id
+        WHERE study_groups.id = $1
+      `,
+        [studyGroupId]
+      );
+
+      res.status(200).json({ body: result.rows });
+      client.release();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error, body: [] });
+    }
   })
   .get("/admins/:id", async (req: Request, res: Response) => {
     const studyGroupId = req.params.id;
-    const client = await pool.connect();
 
-    const result = await client.query(
-      `
-      SELECT users.id as "userID", users.username as "username" FROM users
-      INNER JOIN study_group_admins
-      ON users.id = study_group_admins.user_id
-      INNER JOIN study_groups 
-      ON study_groups.id = study_group_admins.study_group_id
-      WHERE study_groups.id = $1
-    `,
-      [studyGroupId]
-    );
+    try {
+      const client = await pool.connect();
 
-    res.json({ authenticated: true, body: result.rows });
-    client.release();
+      const result = await client.query(
+        `
+        SELECT users.id as "userID", users.username as "username" FROM users
+        INNER JOIN study_group_admins
+        ON users.id = study_group_admins.user_id
+        INNER JOIN study_groups 
+        ON study_groups.id = study_group_admins.study_group_id
+        WHERE study_groups.id = $1
+      `,
+        [studyGroupId]
+      );
+
+      res.status(200).json({ body: result.rows });
+      client.release();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error, body: [] });
+    }
   })
   .post("/shared-notes/new", async (req: Request, res: Response) => {
     const { studyGroupId, studyNoteId } = req.body;
-    const client = await pool.connect();
 
-    const result = await client.query(
-      `
-    INSERT INTO shared_notes (study_group_id, study_note_Id)
-    VALUES ($1, $2)
-    RETURNING id
-    `,
-      [studyGroupId, studyNoteId]
-    );
+    try {
+      const client = await pool.connect();
 
-    const id = result.rows[0].id;
+      const result = await client.query(
+        `
+      INSERT INTO shared_notes (study_group_id, study_note_Id)
+      VALUES ($1, $2)
+      RETURNING id
+      `,
+        [studyGroupId, studyNoteId]
+      );
 
-    res.json({ authenticated: true, body: id });
-    client.release();
+      const id = result.rows[0].id;
+
+      res.status(201).json({ body: id });
+      client.release();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error, body: [] });
+    }
   })
   .delete("/shared-notes/delete", async (req: Request, res: Response) => {
     const { id: sharedNoteID } = req.body;
-    const client = await pool.connect();
 
-    console.log(sharedNoteID);
+    try {
+      const client = await pool.connect();
 
-    const result = await client.query(
-      `
-    DELETE FROM shared_notes
-    WHERE id = $1
-    `,
-      [sharedNoteID]
-    );
+      console.log(sharedNoteID);
 
-    res.json({ authenticated: true, body: null });
-    client.release();
+      const result = await client.query(
+        `
+      DELETE FROM shared_notes
+      WHERE id = $1
+      `,
+        [sharedNoteID]
+      );
+
+      res.status(204).json({ body: [] });
+      client.release();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error, body: [] });
+    }
   })
   .post("/shared-notes/:id", async (req: Request, res: Response) => {
     const { userId } = req.body;
     const studyGroupId = req.params.id;
-    const client = await pool.connect();
 
-    const result = await client.query(
-      `
-    SELECT study_notes.id as "studyNoteId", shared_notes.id as "id"
-    FROM study_groups
-    INNER JOIN shared_notes
-    ON study_groups.id = shared_notes.study_group_id
-    INNER JOIN study_notes
-    ON study_notes.id = shared_notes.study_note_id
-    INNER JOIN users
-    ON study_notes.user_id = users.id
-    WHERE users.id = $1 AND study_groups.id = $2
-    `,
-      [userId, studyGroupId]
-    );
+    try {
+      const client = await pool.connect();
 
-    res.json({ authenticated: true, body: result.rows });
-    client.release();
+      const result = await client.query(
+        `
+      SELECT study_notes.id as "studyNoteId", shared_notes.id as "id"
+      FROM study_groups
+      INNER JOIN shared_notes
+      ON study_groups.id = shared_notes.study_group_id
+      INNER JOIN study_notes
+      ON study_notes.id = shared_notes.study_note_id
+      INNER JOIN users
+      ON study_notes.user_id = users.id
+      WHERE users.id = $1 AND study_groups.id = $2
+      `,
+        [userId, studyGroupId]
+      );
+
+      res.status(200).json({ body: result.rows });
+      client.release();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error, body: [] });
+    }
   })
-  .post("/leave", async (req: Request, res: Response) => {
+  .delete("/leave", async (req: Request, res: Response) => {
     const { studyGroupId, userId } = req.body;
-    const client = await pool.connect();
+    try {
+      const client = await pool.connect();
 
-    const result = await client.query(
-      `
-      DELETE FROM study_group_members
-      WHERE study_group_id = $1 AND user_id = $2
-    `,
-      [studyGroupId, userId]
-    );
+      const result = await client.query(
+        `
+        DELETE FROM study_group_members
+        WHERE study_group_id = $1 AND user_id = $2
+      `,
+        [studyGroupId, userId]
+      );
 
-    res.json({ authenticated: true, body: [] });
-    client.release();
+      res.status(204).json({ body: [] });
+      client.release();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error, body: [] });
+    }
   })
   .post("/join", async (req: Request, res: Response) => {
     const { studyGroupId, userId } = req.body;
     const client = await pool.connect();
 
-    const result = await client.query(
-      `
-      INSERT INTO study_group_members (study_group_id, user_id)
-      VALUES ($1, $2)
-    `,
-      [studyGroupId, userId]
-    );
+    try {
+      const result = await client.query(
+        `
+        INSERT INTO study_group_members (study_group_id, user_id)
+        VALUES ($1, $2)
+      `,
+        [studyGroupId, userId]
+      );
 
-    res.json({ authenticated: true, body: [] });
-    client.release();
+      res.status(201).json({ body: [] });
+      client.release();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error, body: [] });
+    }
   })
   .get("/:id", async (req: Request, res: Response) => {
     const id = req.params.id;
 
-    const client = await pool.connect();
-    const result = await client.query(
-      `
-    SELECT study_groups.name, study_groups.description  FROM study_groups WHERE study_groups.id = $1
-    `,
-      [id]
-    );
-    res.json({ authenticated: true, body: result.rows });
-    client.release();
+    try {
+      const client = await pool.connect();
+      const result = await client.query(
+        `
+      SELECT study_groups.name, study_groups.description  FROM study_groups WHERE study_groups.id = $1
+      `,
+        [id]
+      );
+      res.status(200).json({ body: result.rows });
+      client.release();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error, body: [] });
+    }
   });
 
 function logger(req: Request, res: Response, next: NextFunction) {
